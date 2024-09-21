baa9818a38f2d3cca8f6d0e43fefc8bcf06c002b
jayeshnara <jayeshnara90@gmail.com>
2024-09-21 16:55:16 +0530

    Cursor Animation

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-var tl = gsap.timeline();
-tl.from(".line h1", {
-  y: 120,
-  stagger: 0.1,
-  duration: 0.6,
-  delay: 0.5,
-});
-tl.from("#line1-part1 ", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
-    setInterval(function () {
-      if (grow <= 100) {
-        h5timer.innerHTML = grow++;
-      } else {
-        grow = 100;
-        console.log();
-      }
-    }, 35);
-  },
-});
-tl.to('.line  h2',{
-  animationName:'anime',
-  opacity:1
-})
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.4,
-  delay: 4,
-});
-tl.from('#page1',{
-  delay:0.2,
-  y:1600,
-  opacity:0,
-  ease:Power4
-})
-tl.to('#loader',{
-  display:none,
-})
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 120,
+    stagger: 0.1,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1 ", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow <= 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          grow = 100;
+          // console.log();
+        }
+      }, 35);
+    },
+  });
+  tl.to(".line  h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4,
+  });
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+// loadingAnimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+}
+cursorAnimation();
+
+Shery.makeMagnet("#nav-part2 p ,#navi", {});
